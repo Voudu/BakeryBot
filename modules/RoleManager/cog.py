362ef7f8e5237ec9138485670f91fdef2d147366
Voudu <alexescatel9@gmail.com>
2022-03-20 16:46:19 -0500

    Removed channelId checking in rolemanager

--- modules/RoleManager/cog.py
+++ modules/RoleManager/cog.py
@@ -32,7 +32,7 @@ class RoleManager(commands.Cog, name="Role Manager Cog"):
     def __init__(self, bot: commands.Bot):
         
         self.bot = bot
-        self.channelId = 826123311758442516
+        # self.channelId = 826123311758442516 <- feature deprecated due to complexity
         self.msg_role_dict = {}    # nested dictionary, maps message ids to a dict of reactions and roles which it listens for
                                     #
                                     # looks like this
@@ -63,11 +63,11 @@ class RoleManager(commands.Cog, name="Role Manager Cog"):
         start the process of creating a block which we can add reacts to - to assign roles
         """
 
-        channelObj = self.bot.get_channel(self.channelId)
-        if channelObj == None:
-            print("Default channel set doesn't exist - please setup a new channel")
-            await channelObj.send(f"Default channel with ID {self.channelId} does not exist - set a new channel with !rolesetchannel")
-            return
+        channelObj = ctx.channel
+        # if channelObj == None:
+        #     print("Default channel set doesn't exist - please setup a new channel")
+        #     await channelObj.send(f"Default channel with ID {self.channelId} does not exist - set a new channel with !rolesetchannel")
+        #     return
 
         try:
             regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
@@ -150,7 +150,7 @@ class RoleManager(commands.Cog, name="Role Manager Cog"):
         if not self.msg_role_dict:                                             # dictionary is empty, make sure theres no data to load first
             self.loadObj(ctx)
 
-        channelObj = self.bot.get_channel(self.channelId)
+        channelObj = ctx.channel
         msgId = str(msg_id)
 
         #                                                                        # role gets passed like <@&932015848489623682>
@@ -192,22 +192,35 @@ class RoleManager(commands.Cog, name="Role Manager Cog"):
                 except Exception as e:
                     print(e)
 
-    @commands.command(pass_context=True)
-    @commands.has_permissions(administrator=True)       # make sure the person executing has permissions
-    async def rolesetchannel(self, ctx: commands.Context, arg1):
-        """
-        !rolesetchannel #channel_name
-        """
-        pass
+    # @commands.command(pass_context=True)
+    # @commands.has_permissions(administrator=True)       # make sure the person executing has permissions
+    # async def rolesetchannel(self, ctx: commands.Context, channel):
+    #     """
+    #     !rolesetchannel #channel_name
+    #     """
+    #     # split off channel input to isolate the id
+    #     channel_split = channel.split("#", 1)
+    #     if len(channel_split) > 1:
+    #         channel_split = channel_split[1].split(">", 1)
+    #         channel_id = channel_split[0]
+    #     else:
+    #         print("Invalid channel provide")
+    #         await ctx.channel.send("RoleManager: Error: use a text channel to set a role channel")
+    #         return
+
+    #     guild = ctx.guild
+
+    #     if (lambda c: c.id == channel_id, guild.text_channels):
+    #         self.channelId = channel_id
+    #         await ctx.channel.send("RoleManager: channel successfully set!")
+    #     else:
+    #         await ctx.channel.send("RoleManager: Error: channel selected does not exist, try another.")
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
 
         if payload.member == self.bot.user:
             print("Ignored bot adding emoji")
-            return
-        if payload.channel_id != self.channelId:
-            print("Ignored reaction not in role manager channel")
             return
 
         guild_id = payload.guild_id         # get the guild id so we know which file to open
@@ -241,8 +254,45 @@ class RoleManager(commands.Cog, name="Role Manager Cog"):
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
-        msgId = payload.message_id
-        print(msgId)
+
+        if payload.member == self.bot.user:
+            print("Ignored bot removing emoji")
+            return
+
+        guild_id = payload.guild_id
+
+        if not self.msg_role_dict:
+            try:                            # try to load the dictionary from the json file
+                with open(f'modules/RoleManager/dat/{guild_id}.json') as f:
+                    self.msg_role_dict = json.load(f)
+                    print(self.msg_role_dict)
+                    print(f"msg_role_dict loaded as: {type(self.msg_role_dict)}")
+            except FileNotFoundError as fe:
+                print(fe)
+
+        msgId = str(payload.message_id)
+        if msgId in self.msg_role_dict:
+            userId = payload.user_id
+
+            guild = self.bot.get_guild(guild_id)
+            member = guild.get_member(userId)
+
+            reaction = str(payload.emoji)
+            reaction = emoji.demojize(reaction)
+
+            if reaction in self.msg_role_dict[msgId]:
+                role_id = self.msg_role_dict[msgId][reaction]
+                role = guild.get_role(role_id)
+                for r in member.roles:
+                    if r == role:
+                        await member.remove_roles(role)
+                        return
+            else:
+                print("Emoji not found as a key in this msgId")
+        else:
+            print("msgId not found")
+                
+
 
     # load data from a file, json
     def loadObj(self, ctx):
