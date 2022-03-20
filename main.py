288a7ee584aa45e1fa3e5b26fd6af97a4ab76e6f
Voudu <alexescatel9@gmail.com>
2022-03-20 15:48:20 -0500

    moved all bot code to a class with some new functions. Also moved some more vars to env

--- main.py
+++ main.py
@@ -5,42 +5,34 @@ import sys
 import os
 import discord
 
-# TODO:
-#   - make this into a class
-
 load_dotenv()
 token = os.getenv("TOKEN")
+app_id = os.getenv("APP_ID")
+guild_id = os.getenv("GUILD_ID")
 
 sys.path.append(".")
 
-def main():
-    client = commands.Bot(
-        command_prefix="!",
-        intents=discord.Intents.all(),
-        application_id=os.getenv("APP_ID"),
-        test_guilds=[633847600403054592]
-    )
-
-    @client.event
-    async def on_ready():
-        print(f"{client.user.name} has connected to discord")
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix="!",
+            intents=discord.Intents.all(),
+            application_id=app_id
+        )
 
     async def setup_hook(self):
         for folder in os.listdir("modules"):
             if os.path.exists(os.path.join("modules", folder, "cog.py")):
-                await client.load_extension(f"modules.{folder}.cog")
+                await self.load_extension(f"modules.{folder}.cog")
 
-    # TODO
-    # await bot.tree.sync(guild = discord.Object(id= guild_id))
+        await self.tree.sync(guild = discord.Object(id=guild_id))
 
-    # TODO
-    # implement the following
     # async def close(self):
-    #   await super().close()
-    #   await self.sessions.close()
+    #     await super().close()
+    #     await self.session.close()
 
-    # should learn how to move this to an environment variable
-    client.run(token)
+    async def on_ready(self):
+        print(f"{self.user.name} has connected to discord")
 
-if __name__ == '__main__':
-    main()
+bot = MyBot()
+bot.run(token)
